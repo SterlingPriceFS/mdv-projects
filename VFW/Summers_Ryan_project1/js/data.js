26f2651928442a076b11ec8f4abbbacaa19ff488
Ryan Summers <sterlingprice@fullsail.edu>
2011-12-03 20:59:08 -0800

    Adding strings to local storage

--- VFW/Summers_Ryan_project1/js/data.js
+++ VFW/Summers_Ryan_project1/js/data.js
@@ -25,12 +25,31 @@ window.addEventListener("DOMContentLoaded", function(){
 		}
 		selectLi.appendChild(makeSelect);
 	};
-	
-	
-	//Variable Defaults
-	var skillSet = [ " ", "strength", "intelligence", "endurance" ];
-	
-	makeSkillSet();
+
+//declare items for dynamically added list
+var skillSet = [ "Random", "Strength", "Intelligence", "Endurance" ];	
+
+
+	function saveData() {
+		var id	= Math.floor(Math.random()*100001);
+		//gather up all our form field values and store in an object.
+		//object properties contain array with label AND user input value
+		var quest = {};
+			quest.name = ["Title", $("task").value];							//creates an array inside the object for each field on our form
+			quest.description = ["Description", $("description").value];		//ultimately this object will hold arrays, each array will have
+			quest.dueDate = ["Due Date", $("duedate").value];					//a label for the form field, and the user input of the data
+/* 			quest.dueTime = ["Specific Time", specificValue]; */
+			quest.time = ["Time", $("time").value];
+			quest.importance = ["Importance", $("importance").value];
+			quest.skill = ["Skill Used", $("skill").value];
+			quest.reward = ["Reward", $("reward").value];
+			quest.difficulty = ["Difficulty", $("difficulty").value];
+			quest.notes = ["Notes", $("notes").value];
+		localStorage.setItem(id, JSON.stringify(quest));
+		alert("Quest has been logged!");
+}
+
+makeSkillSet();
 	
 	//Set Link& Submit CLick Events
 /*
@@ -38,11 +57,13 @@ window.addEventListener("DOMContentLoaded", function(){
 	displayLink.addEventListener("click", getData); //getData will be a function
 	var clearLink = $("clear");
 	clearLink.addEventListener("click", clearLocal); //clearLocal will clear local data
-	var save = $("submit");
-	save.addEventListener("click", saveData);
 	var reset = $("reset");
 	reset.addEventListener("click", resetQuest); //will only clear all fields, not local storage
 */
+
+	var save = $("submit");
+	save.addEventListener("click", saveData);  //saveData function will execute when clicked
+
 
 	
 }); 
