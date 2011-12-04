75429f83246994010cc4f1fa98b9f3ebef82ef6f
Ryan Summers <sterlingprice@fullsail.edu>
2011-12-03 19:41:27 -0800

    Adding JS
    Getting ready to delete the Skill id, wanted to save first

--- VFW/Summers_Ryan_project1/js/data.js
+++ VFW/Summers_Ryan_project1/js/data.js
@@ -10,20 +10,37 @@ window.addEventListener("DOMContentLoaded", function(){
 		return theElement;	
 	};
 	
+	//Create select field eldment and populate it with options
+	function makeSkillSet(){
+		var formTag = document.getElementsByTagName("form"),	//formTag is an array, access the form in my HTML document
+			selectLi =$("skill"),								// selects the list item title skilled (in my code I haven't deleted it yet)
+			makeSelect = document.createElement("select");		// creates a select field
+			makeSelect.setAttribute("id", "skill");				// enables us to assign an id of skill to the select field
+		for(var i=0, j=skillSet.length; i<j; i++) {				// for loop cycles through array skillSet
+			var makeOption = document.createElement("option");	// for each item in array, creates element option
+			var optText = skillSet[i];							// accesses array we declared in JS
+			makeOption.setAttribute("value", optText);			// populates that element option with a value of "optText"
+			makeOption.innerHTML = optText;						// sets optText to value of array, creating the element, it's floating in JS
+			makeSelect.appendChild(makeOption);					// appends the created object (option) to the select element
+		}
+		selectLi.appendChild(makeSelect)
+	}
+	
 	
 	//Variable Defaults
-	var skillSet = [ "appchoice", "strength", "intelligence", "endurance" ];
+	var skillSet = [ "ignored", "strength", "intelligence", "endurance" ];
 	
+	makeSkillSet();
 	
 	//Set Link& Submit CLick Events
 	var displayLink = $("displayLink");
 	displayLink.addEventListener("click", getData); //getData will be a function
 	var clearLink = $("clear");
-	clearLink.addEventListener("click", clearLocal);
-		
-	
-	
-	
-	
+	clearLink.addEventListener("click", clearLocal); //clearLocal will clear local data
+	var save = $("submit");
+	save.addEventListener("click", saveData);
+	var reset = $("reset");
+	reset.addEventListener("click", resetQuest); //will only clear all fields, not local storage
+
 	
 }); 
